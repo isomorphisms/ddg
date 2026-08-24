cf28fc370ffd3fb864c60604416fd7834caeffe5
i <isomorphisms@sdf.org>
2026-08-24 02:10:07 -0400

    test: scaffold prefetch module test

---
+++ prefetch/prefetch-impl/src/test/java/com/duckduckgo/prefetch/impl/PrefetchSmokeTest.kt
@@ -0,0 +1,11 @@
+package com.duckduckgo.prefetch.impl
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class PrefetchSmokeTest {
+    @Test
+    fun placeholder() {
+        assertTrue(true)
+    }
+}
